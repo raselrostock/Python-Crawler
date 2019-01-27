bc318413e6259af7085162b0e01664342a91e0f1
raselrostock <raselrostock@gmail.com>
2019-01-27 11:13:36 +0100

    Added feature of queue and crawled link in main file

--- main.py
+++ main.py
@@ -7,4 +7,20 @@ def create_project_dir(directory):
         print('Creating project '+ directory)
         os.makedirs(directory)
 
-create_project_dir('newboston')
+# Create queue and crawled files
+def  create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+# Create a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+#create_project_dir('newboston')
+create_data_files('newboston', 'https://thenewboston.com/')
